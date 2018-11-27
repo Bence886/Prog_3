47c14f2f1c660328c39054b0924815f2ea3bfadb
Tóth Bence <tbencet96@gmail.com>
2018-11-27 23:22:07 +0100

    Minta_zh

---
+++ MintaZH_2/MintaZH_2/StringProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MintaZH_2
+{
+    public class StringProcessor
+    {
+        public int AllTimeouts { get; set; }
+        private string to = "Request timed out.";
+        private object alltoLock = new object();
+        public int IsTimedOut(string input)
+        {
+            if (input.Contains(to))
+            {
+                string[] lines = input.Split('\n');
+                int tos = lines.Where(x => x.Contains(to)).Count();
+                lock (alltoLock)
+                {
+                    AllTimeouts += tos;
+                }
+                return tos;
+            }
+            return 0;
+        }
+
+
+    }
+}
---
+++ MintaZH_2/Minta_Test/TestClass.cs
@@ -0,0 +1,35 @@
+﻿using MintaZH_2;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minta_Test
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [TestCaseSource(nameof(testSourceString))]
+        public void Test_0(string input, int expected)
+        {
+            StringProcessor sp = new StringProcessor();
+
+            int actual = sp.IsTimedOut(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        public static IEnumerable<object> testSourceString
+        {
+            get
+            {
+                yield return new object[] { "Request timed out.\n, Request timed out.\n, Request timed out.\n, Request timed out.", 4 };
+                yield return new object[] { "asdasdasd", 0 };
+                yield return new object[] { "Request timed out.", 1 };
+                yield return new object[] { "asdRequest timed out.asd", 1 };
+            }
+        }
+    }
+}
